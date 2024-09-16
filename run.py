41761b7485413a313d2235a59463e1a49c9dfd52
Cian Scannell <cianmscannell@gmail.com>
2024-09-16 11:17:46 +0200

    working cine example

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ cine_seq = CineSeries(cine_dir)
 cine_segmentation = cine_seq.predict_segmentation(WANDB_RUN_PATH)
 # number of slices is first?
 
-cine_rvs = cine_seq.get_rv_insertion_points()
+# cine_rvs = cine_seq.get_rv_insertion_points()
 
 # lge_dir = Path(list(patient.glob("*[sS][cC][aA][rR]*"))[0])
 # lge_seq = BaseSequence(lge_dir)
@@ -40,31 +40,31 @@ from skimage import measure
 # contour_scar = measure.find_contours(test_scar2)[0]
 # plt.imshow(test_myo1, cmap="gray")
 # plt.show()
-contour_lv = measure.find_contours(cine_segmentation[8, 10] == 1)[0]
-contour_myo = measure.find_contours(cine_segmentation[8, 10] == 2)[0]
-contour_rv = measure.find_contours(cine_segmentation[8, 10] == 3)[0]
+contour_lv = measure.find_contours(cine_segmentation[5, 1] == 1)[0]
+contour_myo = measure.find_contours(cine_segmentation[5, 1] == 2)[0]
+contour_rv = measure.find_contours(cine_segmentation[5, 1] == 3)[0]
 
-import imgaug.augmenters as iaa
-
-
-def get_offset(seg, RV):
-    RVinsertionx = RV[0]
-    RVinsertiony = RV[1]
-    [xs, ys] = np.where(seg > 0)
-    centx = np.mean(xs)
-    centy = np.mean(ys)
-
-    spoke1m = (centy - RVinsertiony) / (centx - RVinsertionx)
-
-    return np.arctan(spoke1m)
+# import imgaug.augmenters as iaa
 
 
-def rotate_to_rv(image, rv, interp_order=3):
-    angle = np.pi - get_offset(image > 0, rv)
-    rotate_im = iaa.Affine(rotate=np.rad2deg(angle), order=interp_order)
-    rotated_image = rotate_im.augment_image(image)
+# def get_offset(seg, RV):
+#     RVinsertionx = RV[0]
+#     RVinsertiony = RV[1]
+#     [xs, ys] = np.where(seg > 0)
+#     centx = np.mean(xs)
+#     centy = np.mean(ys)
 
-    return rotated_image
+#     spoke1m = (centy - RVinsertiony) / (centx - RVinsertionx)
+
+#     return np.arctan(spoke1m)
+
+
+# def rotate_to_rv(image, rv, interp_order=3):
+#     angle = np.pi - get_offset(image > 0, rv)
+#     rotate_im = iaa.Affine(rotate=np.rad2deg(angle), order=interp_order)
+#     rotated_image = rotate_im.augment_image(image)
+
+#     return rotated_image
 
 
 # plt.subplot(1, 2, 1)
@@ -83,8 +83,8 @@ fig, ax = plt.subplots()
 ax.imshow(cine_seq.slice_data["slice06"]["pixel_array"][1] / 300, cmap="gray")
 # ax.imshow(lge_seq.slice_data["slice06"]["pixel_array"][1] / 300, cmap="gray")
 
-# ax.plot(contour_rv[:, 1], contour_rv[:, 0], linewidth=2.5, color="tab:blue")
-# ax.plot(contour_lv[:, 1], contour_lv[:, 0], linewidth=2.5, color="tab:green")
+ax.plot(contour_rv[:, 1], contour_rv[:, 0], linewidth=2.5, color="tab:blue")
+ax.plot(contour_lv[:, 1], contour_lv[:, 0], linewidth=2.5, color="tab:green")
 ax.plot(contour_myo[:, 1], contour_myo[:, 0], linewidth=2.5, color="tab:orange")
 # ax.plot(contour_epi[:, 1], contour_epi[:, 0], linewidth=2.5, color="tab:orange")
 # ax.plot(contour_endo[:, 1], contour_endo[:, 0], linewidth=2.5, color="tab:orange")
